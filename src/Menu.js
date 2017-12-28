51df1aa4e45b09b57a0ad95a3111ecacc9e48bb3
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2017-12-28 12:05:31 +0200

    Fixed a wrong margin top (spelling error)

--- src/Menu.js
+++ src/Menu.js
@@ -4,7 +4,7 @@ class Menu extends Component {
   render() {
     return(
       <nav className="navbar navbar-default">
-        <div className="col-sm-7 col-sm-offset-2" style={{martinTop: 15}}>
+        <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
           MENU
         </div>
       </nav>
